32fb0d0371d904b538576f8f2780ab965753ad05
Michael Karcher <github@mkarcher.dialup.fu-berlin.de>
2025-08-25 23:58:29 -0800

    Add copy_to_user test for Niagara

--- cfutest.c
+++ cfutest.c
@@ -21,6 +21,9 @@ size_t NG4copy_from_user(void* dest, const void* src, size_t len);
 size_t GENcopy_to_user(void* dest, const void* src, size_t len);
 size_t U1copy_to_user(void* dest, const void* src, size_t len);
 size_t U3copy_to_user(void* dest, const void* src, size_t len);
+size_t NGcopy_to_user(void* dest, const void* src, size_t len);
+size_t NG2copy_to_user(void* dest, const void* src, size_t len);
+size_t NG4copy_to_user(void* dest, const void* src, size_t len);
 
 typedef size_t copy_fn(void*, const void*, size_t);
 copy_fn *copy_to_user = GENcopy_to_user;
@@ -121,12 +124,16 @@ int main(int argc, char** argv)
         } else if(!strcmp(argv[1], "--u3")){
             copy_from_user = U3copy_from_user;
             copy_to_user = U3copy_to_user;
-        } else if(!strcmp(argv[1], "--ng"))
+        } else if(!strcmp(argv[1], "--ng")) {
             copy_from_user = NGcopy_from_user;
-        else if(!strcmp(argv[1], "--ng2"))
+            copy_to_user = NGcopy_to_user;
+        } else if(!strcmp(argv[1], "--ng2")) {
             copy_from_user = NG2copy_from_user;
-        else if(!strcmp(argv[1], "--ng4"))
+            copy_to_user = NG2copy_to_user;
+        } else if(!strcmp(argv[1], "--ng4")) {
             copy_from_user = NG4copy_from_user;
+            copy_to_user = NG4copy_to_user;
+        }
         else
         {
             fputs("Bad option, use --gen, --u1,  --u3 (default), --ng, --ng2 or --ng4\n", stderr);
